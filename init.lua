0b9b576d2d2b5d98aeb1d40e666a08b2982cd65d
Atavixion <jukiangm@gmail.com>
2023-12-05 17:51:47 +0200

    🔧 Move autocommands to init.lua

--- init.lua
+++ init.lua
@@ -63,5 +63,25 @@ require 'commands'
 vim.cmd.colorscheme('MonokaiPro')
 require 'colorizer'.setup()
 
+-- [[ Autocommands ]]
+-- Highlight on yank
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*'
+})
+
+-- autocd if opening a dir
+local cdpwd = vim.api.nvim_create_augroup('cdpwd', { clear = true })
+vim.api.nvim_create_autocmd({ 'VimEnter' }, {
+  group = cdpwd,
+  pattern = '*',
+  command = 'cd ' .. vim.fn.getcwd()
+})
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- lua/commands.lua
+++ lua/commands.lua
@@ -16,24 +16,4 @@ vim.api.nvim_create_user_command('Gc',
   {}
 )
 
--- [[ Highlight on yank ]]
--- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = highlight_group,
-  pattern = '*'
-})
-
--- autocd if opening a dir
-local cdpwd = vim.api.nvim_create_augroup('cdpwd', { clear = true })
-
-vim.api.nvim_create_autocmd({ 'VimEnter' }, {
-  group = cdpwd,
-  pattern = '*',
-  command = 'cd ' .. vim.fn.getcwd()
-})
-
 -- vim: ts=2 sts=2 sw=2 et
