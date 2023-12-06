0b859bd239c74180e461c9bdd499a5006d6ebf42
Atavixion <jukiangm@gmail.com>
2023-12-06 12:32:21 +0200

    🔥 Remove keybinds bcs they dont work

--- lua/plugins/cheatsheet.lua
+++ lua/plugins/cheatsheet.lua
@@ -1,14 +1,9 @@
 -- creates a cheatsheet to easily lookup commands
 return {
   'doctorfree/cheatsheet.nvim',
-
   dependencies = {
     'nvim-telescope/telescope.nvim',
     'nvim-lua/popup.nvim',
     'nvim-lua/plenary.nvim',
-  },
-
-  opts = {
-    telescope_mappings = {}
   },
 }
