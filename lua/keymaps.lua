e8f4b049ed65c8a7af5f58abecd61700ecb89ad5
Atavixion <jukiangm@gmail.com>
2023-12-08 12:31:13 +0200

    ♻️  Create alias for vim.keymap.set

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,24 +1,25 @@
 -- [[ Basic Keymaps ]]
+local map = vim.keymap.set
 
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
-vim.keymap.set({ 'n', 'v' }, '<leader>', '<Nop>', { silent = true })
+map({ 'n', 'v' }, '<leader>', '<Nop>', { silent = true })
 
 -- Remap for dealing with word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+map('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+map('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+map('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+map('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+map('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+map('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
 -- [[ Plugin Keymaps ]]
-vim.keymap.set('n', '<leader>f', "<Cmd>Neotree toggle <CR>", { desc = 'Toggle Neotree' })
-vim.keymap.set('n', '<leader>L', "<Cmd>Lazy <CR>", { desc = 'Open [L]azy.nvim' })
-vim.keymap.set('n', '<leader>gc', "<Cmd>Gcommit <CR>", { desc = 'Add and [c]ommit a file' })
-vim.keymap.set('n', '<leader>?', "<Cmd>Cheatsheet <CR>", { desc = 'Open the cheat sheet' })
-vim.keymap.set({ 'n', 'v' }, '<F8>', "<Cmd>SymbolsOutline <CR>", { desc = 'Toggle symbols outline' })
+map('n', '<leader>f', "<Cmd>Neotree toggle <CR>", { desc = 'Toggle Neotree' })
+map('n', '<leader>L', "<Cmd>Lazy <CR>", { desc = 'Open [L]azy.nvim' })
+map('n', '<leader>gc', "<Cmd>Gcommit <CR>", { desc = 'Add and [c]ommit a file' })
+map('n', '<leader>?', "<Cmd>Cheatsheet <CR>", { desc = 'Open the cheat sheet' })
+map({ 'n', 'v' }, '<F8>', "<Cmd>SymbolsOutline <CR>", { desc = 'Toggle symbols outline' })
 
 -- vim: ts=2 sts=2 sw=2 et
